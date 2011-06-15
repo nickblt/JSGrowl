deb287c542c37e12fe6d4cf7dd0774e9acb5624e
Nicholas Letourneau <nick@pathridden.com>
2011-06-14 17:56:35 -0700

    switched to delegate based image downloading, didn't help with safari crash

--- GrowlNotifier.h
+++ GrowlNotifier.h
@@ -1,12 +1,15 @@
+#import <AppKit/AppKit.h>
+#import <CoreFoundation/CoreFoundation.h>
 #import <Growl/Growl.h>
 
 #define GN_APPLICATION_NAME @"Google Music Notifier"
 #define GN_NOTIFICATION_NAME @"Track Change"
 
-@interface GrowlNotifier :NSObject <GrowlApplicationBridgeDelegate> {} 
+@interface GrowlNotifier :NSObject <GrowlApplicationBridgeDelegate> {
+  CFMutableDictionaryRef connectionToInfoMapping;
+} 
 -(void) growlAlertWithTitle:(NSString *)title message:(NSString *)message;
 -(void) growlAlertWithTitle:(NSString *)title message:(NSString *)message iconURL:(NSString *)url;
 -(BOOL) isGrowlInstalled;
 -(BOOL) isGrowlRunning;
-
 @end
--- GrowlNotifier.m
+++ GrowlNotifier.m
@@ -7,6 +7,10 @@
   if ( (self = [super init]) ) {
     /* Tell growl we are going to use this class to hand growl notifications */
     [GrowlApplicationBridge setGrowlDelegate:self];
+    connectionToInfoMapping = CFDictionaryCreateMutable(kCFAllocatorDefault,
+                                                        0,
+                                                        &kCFTypeDictionaryKeyCallBacks,
+                                                        &kCFTypeDictionaryValueCallBacks);
   }
   return self;
 }
@@ -39,17 +43,78 @@
                                  isSticky:NO
                              clickContext:nil];
 }
-
-
--(void) growlAlertWithTitle:(NSString *)title message:(NSString *)message iconURL:(NSString *)url
+-(void) growlAlertWithTitle:(NSString *)title message:(NSString *)message icon:(NSData *)icon
 {
   [GrowlApplicationBridge notifyWithTitle:title
                               description:message
                          notificationName:GN_NOTIFICATION_NAME
-                                 iconData:nil
+                                 iconData:icon
                                  priority:0
                                  isSticky:NO
                              clickContext:nil];
+}
+
+-(void) growlAlertWithTitle:(NSString *)title message:(NSString *)message iconURL:(NSString *)url
+{
+  NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]
+                                           cachePolicy:NSURLRequestReturnCacheDataElseLoad
+                                       timeoutInterval:3.0];
+
+  NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+ 
+  if (connection) {
+    CFDictionaryAddValue(connectionToInfoMapping,
+                         connection,
+                         [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                          [NSMutableData data], 
+                          @"receivedData",
+                          title,
+                          @"title",
+                          message,
+                          @"message",
+                          nil]);
+  } else {
+    [self growlAlertWithTitle:title message:message];
+  }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+  NSMutableDictionary *connectionInfo = CFDictionaryGetValue(connectionToInfoMapping, connection);
+  [[connectionInfo objectForKey:@"receivedData"] setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+  NSMutableDictionary *connectionInfo = CFDictionaryGetValue(connectionToInfoMapping, connection);
+  [[connectionInfo objectForKey:@"receivedData"] appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+
+  [connection release];
+
+  NSMutableDictionary *connectionInfo = CFDictionaryGetValue(connectionToInfoMapping, connection);
+  [[connectionInfo objectForKey:@"receivedData"] release];
+
+  [self growlAlertWithTitle:[connectionInfo objectForKey:@"title"]
+                    message:[connectionInfo objectForKey:@"message"]];  
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+  [connection release];
+
+  NSMutableDictionary *connectionInfo = CFDictionaryGetValue(connectionToInfoMapping, connection);
+  NSImage *icon = [[NSImage alloc] initWithData:[connectionInfo objectForKey:@"receivedData"]];
+
+  [[connectionInfo objectForKey:@"receivedData"] release];
+  
+  [self growlAlertWithTitle:[connectionInfo objectForKey:@"title"]
+                    message:[connectionInfo objectForKey:@"message"]
+                       icon:[NSData dataWithData:[icon TIFFRepresentation]]];
+  [icon release];
 }
 
 -(BOOL) isGrowlInstalled
